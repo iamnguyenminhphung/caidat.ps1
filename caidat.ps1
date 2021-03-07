fef07721ea048c40ceba56eede581b093aa803e6
iamnguyenminhphung <80209922+iamnguyenminhphung@users.noreply.github.com>
2021-03-07 22:12:41 +0700

    Create caidat.ps1

---
+++ caidat.ps1
@@ -0,0 +1,3 @@
+Frg-ArgSverjnyyCebsvyr -Cebsvyr Qbznva,Choyvp,Cevingr -Ranoyrq Snyfr
+& {$C = $rai:GRZC + '\puebzrerzbgrqrfxgbcubfg.zfv'; Vaibxr-JroErdhrfg 'uggcf://qy.tbbtyr.pbz/rqtrqy/puebzr-erzbgr-qrfxgbc/puebzrerzbgrqrfxgbcubfg.zfv' -BhgSvyr $C; Fgneg-Cebprff $C -Jnvg; Erzbir-Vgrz $C}
+& {$C = $rai:GRZC + '\puebzr_vafgnyyre
